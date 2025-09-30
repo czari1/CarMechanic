d5c44ce0b5c7fca9337f7e9942c075afb3b78e98
czari1 <jaroscezary23@gmail.com>
2025-09-30 15:01:02 +0200

    Database connection

---
+++ Cars.Infrastructure/CarDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Cars.Application.Common;
+using Cars.Infrastructure.Persistance.Const;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Cars.Domain.Entities;
+
+public class CarsContext : DbContext , ICarContext
+{
+    public CarsContext(DbContextOptions<CarsContext> options) : base(options)
+    {
+
+    }
+    public DbSet<Client> Clients { get; set; } = null!;
+    public DbSet<Service> Services { get; set; } = null!;
+
+    public IQueryable<Car> CarQuery => Set<Car>().AsNoTracking().AsQueryable();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(builder);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"CarsConnectionString", x => x.MigrationsHistoryTable("__EFMigrationsHistory", DbConst.CARS_SCHEMA_NAME));
+    } 
+    
+    // fluent validation entityframework (przeczytac)
+    
+}
---
+++ Cars.Infrastructure/Persistance/Const/DbConst.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars.Infrastructure.Persistance.Const
+{
+    public static class DbConst
+    {
+        public const string CARS_SCHEMA_NAME = "Cars";
+    }
+}
