a7d726fa9e5fbee25c72970d61651fcf3728d231
czari1 <jaroscezary23@gmail.com>
2025-10-19 11:50:16 +0200

    Car and clients controllers

---
+++ Cars/Controllers/Clients/CarController.cs
@@ -0,0 +1,53 @@
+﻿using Cars.Application.Clients.AddClient;
+using Cars.Application.Clients.DeleteClient;
+using Cars.Application.Clients.DisplayAllCars;
+using Cars.Application.Clients.GetCarById;
+using Cars.Application.Clients.Models;
+using Cars.Application.Clients.UpdateClient;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cars.Controllers.Clients;
+
+[Route("/cars")]
+
+public class CarController : ControllerBase
+{
+    [HttpGet("cars/{id:int}")]
+    public async Task<ActionResult<CarDto>> GetCarById(int id, CancellationToken ct)
+    {
+        var cmd = new GetCarByIdCommand(id);
+        return Ok();
+    }
+
+    [HttpGet("cars")]
+    public async Task<ActionResult<IReadOnlyCollection<CarListDto>>> DisplayAllCars(CancellationToken ct)
+    {
+        var cmd = new DisplayAllCarsCommand();
+        return Ok();
+    }
+
+    [HttpPost("car")] //Poprawiac wszystko
+    public async Task<IActionResult> AddCar(
+         int id,
+         [FromBody] AddClientCommand cmd,
+         CancellationToken ct)
+    {
+        return NoContent();
+    }
+
+    [HttpDelete("car/{id:int}")]
+    public async Task<IActionResult> DeleteCar(int id, CancellationToken ct)
+    {
+        var cmd = new DeleteClientCommand(id);
+        return NoContent();
+    }
+
+    [HttpPut("car/{id:int}")]
+    public async Task<IActionResult> UpdateCar(
+      int id,
+      [FromBody] UpdateClientCommand cmd,
+      CancellationToken ct)
+    {
+        return NoContent();
+    }
+}
---
+++ Cars/Controllers/Clients/ClientController.cs
@@ -0,0 +1,53 @@
+﻿using Cars.Application.Clients.AddClient;
+using Cars.Application.Clients.DeleteClient;
+using Cars.Application.Clients.Models;
+using Cars.Application.Clients.UpdateClient;
+using Cars.Application.Clients.DisplayAllClients;
+using Cars.Application.Clients.GetClientById;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cars.Controllers.Clients; //Przerobic caly plik zmiana na dto zmiast obiektow
+
+[Route("/cars")]
+
+public class ClientController : ControllerBase
+{
+    [HttpGet("client/{id:int}")]
+    public async Task<ActionResult<ClientDto>> GetClientById(int id, CancellationToken ct)
+    {
+        var cmd = new GetClientByIdCommand(id);
+        return Ok();
+    }
+
+    [HttpGet("clients")]
+    public async Task<ActionResult<IReadOnlyCollection<ClientListDto>>> DisplayAllClients(int id, CancellationToken ct)
+    {
+        var cmd = new DisplayAllClientsCommand();
+        return Ok();
+    }
+
+    [HttpPost("client")] //Poprawiac wszystko
+    public async Task<IActionResult> AddClient(
+         int id,
+         [FromBody] AddClientCommand cmd,
+         CancellationToken ct)
+    {
+        return NoContent();
+    }
+
+    [HttpDelete("client/{id:int}")]
+    public async Task<IActionResult> DeleteClient(int id, CancellationToken ct)
+    {
+        var cmd = new DeleteClientCommand(id);
+        return NoContent();
+    }
+
+    [HttpPut("client/{id:int}")]
+    public async Task<IActionResult> UpdateClient(
+      int id,
+      [FromBody] UpdateClientCommand cmd,
+      CancellationToken ct)
+    {
+        return NoContent();
+    }
+}
